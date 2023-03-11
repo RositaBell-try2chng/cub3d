3f7ea1e5d6a1475e36f3a207da10dc6025633431
Ruslan Andreev <rbell@c2r11s2.42yerevan.am>
2023-03-11 19:00:27 +0400

    make func draw_vis mor simple

--- draws.c
+++ draws.c
@@ -1,52 +1,33 @@
 #include "cub.h"
 
-static void    find_start_end(double wall_h, int *st, int *en)
+static void	find_start_end(double wall_h, int *st, int *en)
 {
-	*st = (384 - ceil(wall_h / 2));
-	*en = (384 + floor(wall_h / 2));
+	*st = (384 - wall_h / 2);
+	*en = (384 + wall_h / 2);
 	if (*en > 768)
 		(*en) = 768;
 }
 
-static void    draw_wall(t_main *M, t_hit *hit)
+void	draw_vis(t_main *M, t_hit *hit)
 {
-	int             wall_start;
-	int             wall_end;
-	unsigned int    *dst;
+	int				y;
+	unsigned int	*dst;
 	double			wall_h;
-	int				i;
+	int				wall_start;
+	int				wall_end;
 
+	y = 0;
 	wall_h = 768.0 * 48 / hit->len;
 	find_start_end(wall_h, &wall_start, &wall_end);
-	i = -1;
-	while (++i + wall_start < wall_end)
-	{
-		if (wall_start + i < 0)
-			continue;
-		dst = get_pxl_adr(&M->mp->vis, hit->cnt, wall_start + i);
-		*dst = get_pxl(wall_h, hit, i);
-	}
-}
-
-void    draw_vis(t_main *M, t_hit *hit)
-{
-	int             y;
-	unsigned int    *dst;
-	t_img			*vis;
-
-	vis = &M->mp->vis;
-	y = 0;
-	while (y < 384)
-	{
-		dst = get_pxl_adr(vis, hit->cnt, y);
-		*dst = UP_COLOR;
-		y++;
-	}
 	while (y < 768)
 	{
-		dst = get_pxl_adr(vis, hit->cnt, y);
-		*dst = DOWN_COLOR;
+		dst = get_pxl_adr(&M->mp->vis, hit->cnt, y);
+		if (y >= wall_start && y < wall_end)
+			*dst = get_pxl(wall_h, hit, y - wall_start);
+		else if (y < 384)
+			*dst = UP_COLOR;
+		else
+			*dst = DOWN_COLOR;
 		y++;
 	}
-	draw_wall(M, hit);
 }
