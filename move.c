c1b7ffc775f1767f17850de5d4d5e0fc2ef28eff
Ruslan Andreev <rbell@c2r11s2.42yerevan.am>
2023-03-13 20:57:59 +0400

    clean move, fix img invert bug

--- move.c
+++ move.c
@@ -1,23 +1,5 @@
 #include "cub.h"
 
-int	define_speed_ang(t_pl *pl, double *p_ang)
-{
-	int	res_w;
-	int	res_d;
-
-	res_w = pl->flg_move_w + pl->flg_move_s;
-	res_d = pl->flg_move_d + pl->flg_move_a;
-	if (res_w == 0 && res_d == 0)
-		return (0);
-	if (res_w == 1 || res_w == -1)
-	{
-		(*p_ang) += 45.0 * res_d * res_w;
-		return (res_w);
-	}
-	(*p_ang) += 90.0;
-	return (res_d);
-}
-
 void	move(t_main *Main, t_pl *pl)
 {
 	int		speed;
@@ -25,13 +7,6 @@ void	move(t_main *Main, t_pl *pl)
 
 	ang = pl->ang;
 	speed = define_speed_ang(pl, &ang);
-//	if (pl->flg_move_w == 1 && pl->flg_move_s == 0)
-//		move_cnt(Main, pl, pl->ang, 1);
-//	else if (pl->flg_move_w == 0 && pl->flg_move_s == 1)
-//		move_cnt(Main, pl, pl->ang, -1);
-//	if (pl->flg_move_a == 1 && pl->flg_move_d == 0)
-//		move_cnt(Main, pl, pl->ang - 90.0, 1);
-//	else if (pl->flg_move_a == 0 && pl->flg_move_d == 1)
 	move_cnt(Main, pl, ang, speed);
 }
 
