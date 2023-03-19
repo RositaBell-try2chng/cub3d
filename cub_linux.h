849150be0b0bc5165279c117f5b83ab75ccf96a5
Ruslan Andreev <rbell@c2r11s2.42yerevan.am>
2023-03-19 18:10:04 +0400

    enum  bindings

--- cub_linux.h
+++ cub_linux.h
@@ -1,15 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub_parser.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghelman <alan.urman@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 12:46:42 by ghelman           #+#    #+#             */
+/*   Updated: 2023/03/19 12:46:42 by ghelman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB_LINUX_H
 # define CUB_LINUX_H
 
 // # include <X11/X.h> // Seems to be working without this
 # include <X11/keysym.h>
 
-# define ESC XK_Escape
-# define W XK_w
-# define A XK_a
-# define S XK_s
-# define D XK_d
-# define RIGHT_ARROW XK_Right
-# define LEFT_ARROW XK_Left
+enum e_cub_key_bindings {
+	ESC = XK_Escape,
+	W = XK_w,
+	A = XK_a,
+	S = XK_s,
+	D = XK_d,
+	RIGHT_ARROW = XK_Right,
+	LEFT_ARROW = XK_Left,
+};
 
 #endif
